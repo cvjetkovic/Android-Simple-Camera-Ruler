1e53daa7fa76ed1e15aa9870f5145d6804cffd51
cvjetkovic <cvjetkovicvladimir.office@gmail.com>
2020-08-27 13:34:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Ruler.java
@@ -0,0 +1,77 @@
+package com.example.myapplication;
+
+import android.graphics.Point;
+
+import java.util.List;
+
+/**
+ * Created by Vladimir Cvjetkovic
+ */
+public class Ruler {
+
+    private Ruler() {
+    }
+
+    public static double compute(List<Point> points, double scale, int inputUnitIndex, int outputUnitIndex) {
+        if (points.size() < 4) return -1;
+
+        Point ref1 = points.get(0);
+        Point ref2 = points.get(1);
+        Point m1 = points.get(2);
+        Point m2 = points.get(3);
+
+        double reference = getDistance(ref1, ref2);
+        double measurement = getDistance(m1, m2);
+
+        measurement = (measurement * scale) / reference; //Get the actual distance
+        measurement = convertUnits(inputUnitIndex, reference, outputUnitIndex, measurement);
+
+        return measurement;
+    }
+
+    private static double getDistance(Point p1, Point p2) {
+        double x = Math.pow(p2.x - p1.x, 2);
+        double y = Math.pow(p2.y - p1.y, 2);
+        return Math.sqrt(x + y);
+    }
+
+    private static double convertUnits(int refUnit, double reference, int meaUnit, double measurement) {
+        if (refUnit == meaUnit)
+            return measurement;
+
+        measurement = toMeters(measurement, refUnit);
+        switch (meaUnit) {
+            case 0:
+                return measurement;
+            case 1:
+                return Utils.metersToCentimeters(measurement);
+            case 2:
+                return Utils.metersToMillimeters(measurement);
+            case 3:
+                return Utils.metersToInch(measurement);
+            case 4:
+                return Utils.metersToFeet(measurement);
+            case 5:
+                return Utils.metersToYards(measurement);
+            default:
+                return -1;
+        }
+    }
+
+    private static double toMeters(double measurement, int refUnit) {
+        switch (refUnit) {
+            case 0:
+                return measurement;
+            case 1:
+                return Utils.centimetersToMeters(measurement);
+            case 2:
+                return Utils.millimetersToMeters(measurement);
+            case 3:
+                return Utils.inchesToMeters(measurement);
+            case 4:
+                return Utils.yardsToMeters(measurement);
+            default:
+                return -1;
+        }
+    }
+}
